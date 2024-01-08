8e1673f5f1f744efc06d2c25b685cab7efe55e36
Nathan Nicholson <nathan.bytecode@gmail.com>
2024-01-08 14:38:35 +0000

    add home html to blog/templates

--- blog/models.py
+++ blog/models.py
@@ -15,18 +15,29 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
-    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+    
+    def __str__(self):
+        return (f"The title of this post is {self.title} | written by {self.author}") 
 
 class Comment(models.Model):
     post = models.ForeignKey(
         Post,
         on_delete=models.CASCADE,
-        related_name="comments"
+        related_name="blog_comment"
     )
     author = models.ForeignKey(
         User, on_delete=models.CASCADE,
-        related_name="commenter"
+        related_name="comments_author"
     )
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+    
+    def __str__(self):
+        return (f"Comment {self.body} by {self.author}") 
